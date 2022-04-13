50d2ed8610fc32acdc28ad5499ed79b60f75d7a6
Alexey Okhotnikov <deathpressence@gmail.com>
2022-04-13 14:45:11 +0300

    test: environment setup

---
+++ .solcover.js
@@ -0,0 +1,18 @@
+module.exports = {
+  skipFiles: [
+    "entities/ERC20.sol",
+    "entities/ERC20Permit.sol",
+    "entities/Ownable.sol",
+    "interfaces/IERC20.sol",
+    "interfaces/IERC20Metadata.sol",
+    "interfaces/IERC20Permit.sol",
+    "libraries/Address.sol",
+    "libraries/Context.sol",
+    "libraries/Counters.sol",
+    "libraries/SafeERC20.sol",
+    "libraries/Strings.sol",
+    "libraries/cryptography/ECDSA.sol",
+    "libraries/cryptography/EIP712.sol",
+    "mocks/MockToken.sol",
+  ],
+};
---
+++ test/VestingWallet.test.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("VestingWallet", function () {
+  let VestingWalletContract, vestingWallet;
+  let MockTokenContract, mockToken;
+  let owner, beneficiary, user1;
+
+  beforeEach(async function () {
+    MockTokenContract = await ethers.getContractFactory("MockToken");
+    mockToken = await MockTokenContract.deploy("Test token", "TKN", 1000000);
+    await mockToken.deployed();
+
+    [owner, beneficiary, user1] = await ethers.getSigners();
+  });
+
+  describe("constructor", function () {
+    beforeEach(async function () {
+      VestingWalletContract = await ethers.getContractFactory("VestingWallet");
+      vestingWallet = await VestingWalletContract.deploy(
+        mockToken.address,
+        beneficiary.address,
+        Math.ceil(Date.now() / 1000),
+        [1000, 3000, 6000]
+      );
+      await mockToken.deployed();
+    });
+
+    it("Should deploy", async function () {
+      expect(await vestingWallet.address).to.not.equal(
+        ethers.constants.AddressZero
+      );
+    });
+  });
+});
